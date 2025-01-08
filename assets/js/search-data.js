fc7657a84037c6e8d80e6277b7f4e18e87badbf4
sukhwankang <squadrush@naver.com>
2025-01-08 07:48:26 +0000

    Deploying to gh-pages from @ sukhwankang/sukhwankang.github.io@be916b66c9ce2d8e240dc080be1843c3bfc36278 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,6 +422,11 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_2/";
+            },},{id: "news-a-long-announcement-with-details",
+          title: 'A long announcement with details',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_5/";
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
