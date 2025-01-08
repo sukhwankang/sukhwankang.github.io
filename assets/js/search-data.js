0becd8fffc123da4ee830613bb2756d410f5f150
sukhwankang <squadrush@naver.com>
2025-01-08 07:09:47 +0000

    Deploying to gh-pages from @ sukhwankang/sukhwankang.github.io@284a68fc47e1f10885958e5eeb3fc5f561550497 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -417,7 +417,12 @@ ninja.data = [{
           section: "News",},{id: "news-test-announcement",
           title: 'Test announcement 🚧',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "news-test-announcement",
+          title: 'Test announcement',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_5/";
+            },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
           section: "Projects",handler: () => {
