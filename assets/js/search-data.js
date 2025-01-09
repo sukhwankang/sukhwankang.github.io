8dc49cdb396c5dd6a23a7b54f96b8e89baff1d49
sukhwankang <squadrush@naver.com>
2025-01-09 14:24:54 +0000

    Deploying to gh-pages from @ sukhwankang/sukhwankang.github.io@7c0186a4df0ecdbe37c4a931df2dd9a90b79b437 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,40 +407,19 @@ ninja.data = [{
           window.location.href = "/blog/2015/formatting-and-links/";
         
       },
-    },{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
-          description: "",
-          section: "News",},{id: "news-test-announcement",
+    },{id: "news-test-announcement",
           title: 'Test announcement 🚧',
           description: "",
           section: "News",},{id: "news-a-long-announcement-with-details",
           title: 'A long announcement with details',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_5/";
+              window.location.href = "/news/news_2/";
             },},{id: "news-test-announcement",
           title: 'Test announcement 🚧',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_6/";
-            },},{id: "news-test-announcement",
-          title: 'Test announcement 🚧🚧',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_7/";
-            },},{id: "news-test-announcement",
-          title: 'Test announcement 🚧🚧',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_8/";
+              window.location.href = "/news/news_3/";
             },},{id: "projects-project-1",
           title: 'project 1',
           description: "with background image",
